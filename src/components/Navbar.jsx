e5aea6d1ff30dbc5b5c8f6c4c8521614ebee047a
Sachin7717 <sachindhimann71@gmail.com>
2024-07-09 12:25:36 +0530

    checking files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import App from '../App' 
+
+function Navbar() {
+  return (
+    <div>
+        <nav>
+            <h1></h1>
+        </nav>
+
+      
+    </div>
+  )
+}
+
+export default Navbar
